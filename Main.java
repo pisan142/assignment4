af03881ea20674bd7a22230c36578ef922d3d6d9
Yusuf Pisan <pisan@uw.edu>
2021-10-16 14:59:47 -0700

    added Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Placeholder Main.java file for replit.
+ */
+public class Main {
+
+    /**
+     * Call textAnalysis.
+     */
+    public static void main(String[] args) {
+        TextAnalysis.textAnalysis();
+    }
+}
--- TextAnalysis.java
+++ TextAnalysis.java
@@ -5,35 +5,30 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class TextAnalysis
-{
+public class TextAnalysis {
     // provided as an example, should not need it
-    public static void printAlphabet()
-    {
-        for (char curr =  'a'; curr <= 'z'; curr++)
-        {
+    public static void printAlphabet() {
+        for (char curr = 'a'; curr <= 'z'; curr++) {
             System.out.printf("%c ", curr);
         }
     }
 
     // return the number of times c appears in the message
-    public static int countChar(String message, char c)
-    {
+    public static int countChar(String message, char c) {
         return 0;
     }
-    // given a String, 
+
+    // given a String,
     // checks how many times each letter appears and prints it
-    public static void analyzeLine(String message)
-    {
+    public static void analyzeLine(String message) {
     }
+
     // gets input from the user, calls analyzeLine
-    public static void textAnalysis()
-    {
+    public static void textAnalysis() {
 
     }
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         textAnalysis();
     }
 }
