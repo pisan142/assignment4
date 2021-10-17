bfcaf824d7bfd3dd192a871522842dcb4e81b02e
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-10-17 00:38:09 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
   }
   
   /**
-   * This method returns the number of times c appears in the message.
+   * This method should return the number of times c appears in the message.
    * For example, if message = "abcbcbc", and c = 'b', then the method should return 3
    * If no c character appears in the message, return 0
    * NOTE: this method is required for testing, you must not delete it
@@ -29,7 +29,8 @@ public class Main {
 
   /**
    * This method checks how many times each letter appears in the given message and prints it.
-   * For example, if message = "abcbcbc", then the method should print out (output doesn't include "* "):
+   * For example, if message = "abcbcbc", 
+   * then the method should print out (output doesn't include "* "):
    * a: 1
    * b: 3
    * c: 3
@@ -39,9 +40,8 @@ public class Main {
     //TODO
   }
 
-  // This method gets inputs from the user, calls analyzeLine.
   /**
-   * This methods get inputs from user and calls analyzeLine()
+   * This methods get inputs from user and calls analyzeLine().
    * It keeps asking for inputs until user enter "stop"
    * When user enters "stop", the method will end with "All done!"
    */
